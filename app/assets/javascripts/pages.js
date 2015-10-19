0b31e5a00e75ed589b424e76394a8ac624fb4793
Hoggiee <robert.szczygiel22@gmail.com>
2015-10-19 21:34:57 +0000

    laod more show

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,22 +1,35 @@
 $(function() {
-    
 $('.load-more-button').click(function () {
   alert('loadmoarposts!');
+
 });
 
-//find current ID jQuery
+var highestId
 
-var highest = 0, current_id
-$(".post-item").each( function()
-{
-   current_id = parseInt($(this,10).attr('data-id'));
-   if (current_id > highest)
-   {
-      highest = current_id;
+function dataPost(){
+   $.post( "pages/load_more_posts", { highestId: findHighestId() }, function(data, status) {
+   if(status == 'success') {
+      
+   }
+   else {
+   alert('Error!');
    }
 });
-console.log(highest);
-alert(highest)
+   
+   
+   
+}
 
-
+function findHighestId(){
+   var highest = 0, current_id;
+   $(".post-item").each( function()
+   {
+      current_id = parseInt($(this,10).attr('data-id'));
+      if (current_id > highest)
+      {
+         highest = current_id;
+      }
+   });
+   return highest;
+}
 });
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,17 @@
 class PagesController < ApplicationController
     
+    
     def index
         @posts = Post.all.limit(6)
     end
     
     def load_more_posts
-        @posts = Post.all
+        @posts.each do |post|
+            if post[:id] < params[:id]
+                @posts = Post.all.limit(6)
+            end
+        end
+        
         respond_to do |format|
            format.html
            format.json
