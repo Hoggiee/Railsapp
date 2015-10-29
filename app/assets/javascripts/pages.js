83828f94f7f68ea4a661b84a31460ff71afaf6bc
Hoggiee <robert.szczygiel22@gmail.com>
2015-10-29 08:33:13 +0000

    pages.js fix 4

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,6 +1,7 @@
+var highest=0
 $(function() {
 $('.load-more-button').click(function () {
-   var current_id, highest = 0;
+   var current_id;
    $(".post-item").each( function()
    {
       current_id = parseInt($(this,10).attr('data-id'));
@@ -14,9 +15,10 @@ $('.load-more-button').click(function () {
 
 
 function dataPost(){
+   alert('datapossttt');
    $.post( "pages/load_more_posts", { highest_id: highest }, function(data, status) {
    if(status == 'success') {
-   alert('datapost')
+   alert('datapost');
    $.append(data);
    }
    else {
