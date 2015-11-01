bbdb148c414bcd9f4a67641afbe8ff2347949c28
Hoggiee <robert.szczygiel22@gmail.com>
2015-11-01 19:38:43 +0000

    pages.js indent fixes

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,31 +1,30 @@
 $(function() {
-$('.load-more-button').click(function () {
-   dataPost();
-});
-
-
-function dataPost(){
-   $.post( "pages/load_more_posts", { lowest_id: findLowestId() }, function(data, status) {
-   if(status == 'success') {
-   $('.posts-list').append(data);
-   }
-   else {
-   alert('Error!');
-   }
-});
-}
-
-
-function findLowestId(){
-   var current_id, lowest = 10000;
-   $(".post-item").each(function()
-   {
-      current_id = parseInt($(this,10).attr('data-id'));
-      if (current_id < lowest)
-      {
-         lowest = current_id;
+   $('.load-more-button').click(function () {
+      dataPost();
+   });
+   
+   function dataPost(){
+      $.post( "pages/load_more_posts", { lowest_id: findLowestId() }, function(data, status) {
+      if(status == 'success') {
+         $('.posts-list').append(data);
+      }
+      else {
+         alert('Error!');
       }
    });
-   return lowest;
-}
+   }
+   
+   
+   function findLowestId(){
+      var current_id, lowest = 10000;
+      $(".post-item").each(function()
+      {
+         current_id = parseInt($(this,10).attr('data-id'));
+         if (current_id < lowest)
+         {
+            lowest = current_id;
+         }
+      });
+      return lowest;
+   }
 });
